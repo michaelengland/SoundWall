03f80599f0d147c0862f1ba1df46757934bf10ac
Michael England <mg.england@gmail.com>
2013-04-22 01:06:59 +0200

    #3 Animate waveform from right to left.

--- src/main/java/com/github/michaelengland/wallpaper/SoundWallArtistImpl.java
+++ src/main/java/com/github/michaelengland/wallpaper/SoundWallArtistImpl.java
@@ -5,13 +5,26 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.text.TextPaint;
+import android.view.animation.*;
 import com.github.michaelengland.R;
 import com.github.michaelengland.entities.Track;
 
 public class SoundWallArtistImpl implements SoundWallArtist {
+    private static final int WAVEFORM_WIDTH = 1800;
+    private static final int WAVEFORM_HEIGHT = 280;
+    private static final int ANIMATION_DURATION = 8000;
+
     private Paint backgroundPaint;
-    private Paint textPaint;
+    private TextPaint textPaint;
     private Context context;
+    private WallpaperState currentState;
+    private Animation waveformAnimation;
+    private Transformation transformation;
+
+    public SoundWallArtistImpl() {
+        transformation = new Transformation();
+    }
 
     @Override
     public void setContext(Context context) {
@@ -20,9 +33,26 @@ public class SoundWallArtistImpl implements SoundWallArtist {
 
     @Override
     public void draw(WallpaperState state, Canvas canvas) {
+        if ((currentState != null) && (!currentState.equals(state))) {
+            getWaveformAnimation().reset();
+        }
+        currentState = state;
         drawBackground(canvas);
-        drawWaveform(state, canvas);
-        drawTitle(state, canvas);
+        drawWaveform(canvas);
+        drawTitle(canvas);
+    }
+
+    private Animation getWaveformAnimation() {
+        if (waveformAnimation == null) {
+            waveformAnimation = new TranslateAnimation(0, -WAVEFORM_WIDTH, 0, 0);
+            waveformAnimation.setDuration(ANIMATION_DURATION);
+            waveformAnimation.setRepeatCount(TranslateAnimation.INFINITE);
+            waveformAnimation.setRepeatMode(TranslateAnimation.RESTART);
+            waveformAnimation.setStartTime(Animation.START_ON_FIRST_FRAME);
+            waveformAnimation.setInterpolator(new LinearInterpolator());
+            waveformAnimation.initialize(WAVEFORM_WIDTH, WAVEFORM_HEIGHT, WAVEFORM_WIDTH, WAVEFORM_HEIGHT);
+        }
+        return waveformAnimation;
     }
 
     private void drawBackground(Canvas canvas) {
@@ -38,16 +68,16 @@ public class SoundWallArtistImpl implements SoundWallArtist {
         return backgroundPaint;
     }
 
-    private void drawTitle(WallpaperState state, Canvas canvas) {
-        drawText(canvas, titleForState(state), canvas.getWidth() / 2, canvas.getHeight() / 4);
+    private void drawTitle(Canvas canvas) {
+        drawText(canvas, title(), canvas.getWidth() / 2, canvas.getHeight() / 4);
     }
 
-    private String titleForState(WallpaperState state) {
-        if (state.isLoggedIn()) {
-            if (state.isLoadingTrack()) {
+    private String title() {
+        if (currentState.isLoggedIn()) {
+            if (currentState.isLoadingTrack()) {
                 return loadingText();
             }
-            Track track = state.getTrack();
+            Track track = currentState.getTrack();
             if (track == null) {
                 return noTrackText();
             } else {
@@ -78,9 +108,10 @@ public class SoundWallArtistImpl implements SoundWallArtist {
         canvas.drawText(text, x, y, getTextPaint());
     }
 
-    private Paint getTextPaint() {
+    private TextPaint getTextPaint() {
         if (textPaint == null) {
-            textPaint = new Paint();
+            textPaint = new TextPaint();
+            textPaint.setAntiAlias(true);
             textPaint.setColor(Color.RED);
             textPaint.setStyle(Paint.Style.FILL);
             textPaint.setTextSize(20.0f);
@@ -89,12 +120,12 @@ public class SoundWallArtistImpl implements SoundWallArtist {
         return textPaint;
     }
 
-    private void drawWaveform(WallpaperState state, Canvas canvas) {
-        if (state.isLoggedIn() && !state.isLoadingTrack()) {
-            if (state.isLoadingWaveformImage()) {
+    private void drawWaveform(Canvas canvas) {
+        if (currentState.isLoggedIn() && !currentState.isLoadingTrack()) {
+            if (currentState.isLoadingWaveformImage()) {
                 drawLoadingWaveformText(canvas);
             } else {
-                Bitmap waveformImage = state.getWaveformImage();
+                Bitmap waveformImage = currentState.getWaveformImage();
                 if (waveformImage == null) {
                     drawNoWaveformText(canvas);
                 } else {
@@ -125,8 +156,9 @@ public class SoundWallArtistImpl implements SoundWallArtist {
     }
 
     private void drawWaveformImage(Bitmap waveformImage, Canvas canvas) {
-        Bitmap screenBitmap = Bitmap.createScaledBitmap(waveformImage, canvas.getWidth(),
-                canvas.getHeight(), true);
-        canvas.drawBitmap(screenBitmap, 0, 0, getTextPaint());
+        getWaveformAnimation().getTransformation(AnimationUtils.currentAnimationTimeMillis(), transformation);
+        int startY = (canvas.getHeight() / 2) - (waveformImage.getHeight() / 2);
+        transformation.getMatrix().postTranslate(0, startY);
+        canvas.drawBitmap(waveformImage, transformation.getMatrix(), getTextPaint());
     }
 }
--- src/main/java/com/github/michaelengland/wallpaper/WallpaperState.java
+++ src/main/java/com/github/michaelengland/wallpaper/WallpaperState.java
@@ -10,6 +10,23 @@ public class WallpaperState {
     private volatile Bitmap waveformImage;
     private volatile boolean loadingWaveformImage;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WallpaperState that = (WallpaperState) o;
+
+        if (loadingTrack != that.loadingTrack) return false;
+        if (loadingWaveformImage != that.loadingWaveformImage) return false;
+        if (loggedIn != that.loggedIn) return false;
+        if (track != null ? !track.equals(that.track) : that.track != null) return false;
+        if (waveformImage != null ? !waveformImage.equals(that.waveformImage) : that.waveformImage != null)
+            return false;
+
+        return true;
+    }
+
     public boolean isLoggedIn() {
         return loggedIn;
     }
